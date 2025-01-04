7bfe9dda637b5851bab92730aa3df17d8c474812
Gabriel Thiessen <thiessengabriel@gmail.com>
2025-01-03 19:45:53 -0500

    create school

--- Source.c
+++ Source.c
@@ -42,6 +42,35 @@ Course* createCourse() {
     scanf("%s", newCourse->name); //array decays to pointer
     printf("Enter number of students: "); 
     scanf("%u", &(newCourse->totalStudents));
-    return newCourse; 
+    newCourse->studentArray = (Student*)malloc(sizeof(Student) * newCourse->totalStudents);
+
+    for (int i = 0; i < newCourse->totalStudents; i++) { 
+        printf("Enter details for student #%d\n", i + 1); 
+        Student* newStudent = createStudent(); 
+        newCourse->studentArray[i] = *newStudent;
+        free(newStudent);
+    }
+
+    return newCourse;
+
+};
+
+//create a school
+School* createSchool() {
+    School* newSchool = (School*)malloc(sizeof(School));
+    printf("Enter student name: "); 
+    scanf("%s", newSchool->name); //array decays to pointer
+    printf("Enter number of courses: "); 
+    scanf("%u", &(newSchool->totalCourses));
+    newSchool->courseArray = (Course*)malloc(sizeof(Course) * newSchool->totalCourses);
+
+    for (int i = 0; i < newSchool->totalCourses; i++) { 
+        printf("Enter details for course #%d\n", i + 1); 
+        Course* newCourse = createCourse(); 
+        newSchool->courseArray[i] = *newCourse;
+        free(newCourse);
+    }
+
+    return newSchool;
 
 };
